44709236ef95b1bec7b0ef2f1a81238ead4509f0
Guilherme Cintra <guilherme@distrimed.com.br>
2022-11-04 15:03:37 -0300

    Auth google primeira parte

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,61 @@
+import { createContext, ReactNode, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+
+WebBrowser.maybeCompleteAuthSession();
+
+interface UserProps {
+    name: string;
+    avatarUrl: string;
+}
+
+export interface AuthContextDataProps {
+    user: UserProps;
+    isUserLoading: boolean;
+    signIn: () => Promise<void>;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthProviderProps) {
+    const [isUserLoading, setUserLoading] = useState(false);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '72937381771-ogt85ra2fc84pad8i2mphquccpti104h.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    });
+
+
+    async function signIn() {
+        try {
+            setUserLoading(true);
+            await promptAsync();
+        } catch (error) {
+            console.log(error);
+            throw error;
+        } finally {
+            setUserLoading(false);
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            signIn,
+            isUserLoading,
+            user: {
+                name: 'Guilherme Cintra',
+                avatarUrl: 'https://github.com/guilhermedist.png'
+            }
+        }}>
+            {children}
+
+        </AuthContext.Provider>
+    );
+}
