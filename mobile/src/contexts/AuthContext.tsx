ed443104b446f90ec261081770495c53489f72ba
Guilherme Cintra <guilherme@distrimed.com.br>
2022-11-10 16:59:34 -0300

    mobile navigation etre tela do signIn

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { api } from '../services/api';
 
 
 WebBrowser.maybeCompleteAuthSession();
@@ -47,10 +48,22 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
         }
     }
 
-    async function signInWithGoogle(accessToken: string) {
+    async function signInWithGoogle(access_token: string) {
+        try {
+            setUserLoading(true);
 
-        console.log("TOKEN ->", accessToken);
+            const tokenResponse = await api.post('/users', { access_token });
+            api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`;
 
+            const userInfoResponse = await api.get('/me');
+            setUser(userInfoResponse.data.user);
+
+        } catch (error) {
+            console.log(error);
+            throw error;
+        } finally {
+            setUserLoading(false);
+        }
     }
 
     useEffect(() => {
